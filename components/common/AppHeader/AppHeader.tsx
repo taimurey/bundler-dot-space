24b554b2dfb9a891552651c5f368da2e3a1fff69
Taimoor <taimoorshafique541@gmail.com>
2024-03-28 14:03:14 +0500

    Navbar Fix

--- components/common/AppHeader/AppHeader.tsx
+++ components/common/AppHeader/AppHeader.tsx
@@ -43,7 +43,7 @@ const AppHeader: React.FC = () => {
             <Link href="/">
               <h1 className="flex items-center text-lg font-semibold text-white cursor-pointer">
                 <MevLabLogo />
-                <span className="text-[35px] font-normal text-center font-[HeliukBrave] ml-3 uppercase mt-2 tracking-wide">Mevarik</span>
+                <span className="text-[40px] font-normal text-center font-[HeliukBrave] ml-3 uppercase mt-2 tracking-widest">Mevarik</span>
               </h1>
             </Link>
           </div>
