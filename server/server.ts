5accfb4cf5424100a9e7c8f37e0aee428ec0e4fa
Tamioor <taimoorshafique541@gmail.com>
2024-06-04 16:32:13 +0500

    server vercel update

--- server/server.ts
+++ server/server.ts
@@ -2,14 +2,29 @@ require('dotenv').config();
 
 import { SendBundle } from './bundle-sender';
 import express from 'express';
+import cors from 'cors';
 import * as bodyParser from 'body-parser';
-import * as http from 'http';
 import * as path from 'path';
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import { createServer, Server } from 'http';
 
 const app = express();
-const port = 2891;
+
+app.use(
+    cors({
+        origin: [
+            'https://mevarik.com',
+            'https://bundler.space',
+            'http://localhost:3000',
+            'https://bundler-web.vercel.app'
+        ],
+        methods: ["GET", "POST", "PUT", "DELETE", "UPDATE", "PATCH"],
+        credentials: true,
+    })
+);
 
 app.use(bodyParser.json());
+
 app.get('/', (_, res) => {
     const filePath = path.join(__dirname, 'index.html');
     res.sendFile(filePath);
@@ -21,12 +36,6 @@ export interface BundleData {
 }
 
 app.post('/jitoadd', async (req, res) => {
-    // Set CORS headers
-    res.header("Access-Control-Allow-Origin", "https://bundler.space");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE, PATCH");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-
     // Check if the request body is empty
     if (Object.keys(req.body).length === 0) {
         res.status(405).json({ error: 'Not Allowed' });
@@ -44,13 +53,17 @@ app.post('/jitoadd', async (req, res) => {
 
     try {
         await SendBundle(data);
-        res.status(200).json({ success: true });
     } catch (error: any) {
         console.log(error);
         res.status(500).json({ error: `Failed to send bundle: ${error.message}` });
     }
 });
 
-http.createServer(app).listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+let server: Server | null = null;
+
+module.exports = (req: VercelRequest, res: VercelResponse) => {
+    if (server === null) {
+        server = createServer(app);
+    }
+    server.emit('request', req, res);
+};
