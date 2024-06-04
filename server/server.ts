8daa3f61d088173a9a37a5529800c444a1b55527
Tamioor <taimoorshafique541@gmail.com>
2024-06-04 15:36:49 +0500

    cors added

--- server/server.ts
+++ server/server.ts
@@ -12,21 +12,22 @@ const port = 2891;
 
 
 
-app.use(
-    cors({
-      origin: [
-        'https://mevarik.com',
-        'https://bundler.space',
-         'http://localhost:3000',
-      ],  
-      methods: ["GET", "POST", "PUT", "DELETE", "UPDATE", "PATCH"],
-      credentials: true,
-    })
-  );
+// app.use(
+//     cors({
+//       origin: [
+//         'https://mevarik.com',
+//         'https://bundler.space',
+//          'http://localhost:3000',
+//       ],  
+//       methods: ["GET", "POST", "PUT", "DELETE", "UPDATE", "PATCH"],
+//       credentials: true,
+//     })
+//   );
+
+// alow acces from every where
+app.use(cors( {origin: '*'} ));
 
 app.use(bodyParser.json());
-
-
 app.get('/', (_, res) => {
     const filePath = path.join(__dirname, 'index.html');
     res.sendFile(filePath);
